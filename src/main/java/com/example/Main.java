36847cf97e53dc59b048f5f85f7f2ef3ccf2dc5e
Mike Ogar <ogar.m.v@mail.ru>
2024-11-20 17:02:44 +0300

    study tg bot

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -10,6 +10,5 @@ public class Main {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         Bot bot = new Bot();
         telegramBotsApi.registerBot(bot);
-        //
     }
 }
